33c74273b2406b600e2aeed5d02a340f9aa1723f
Jaggy <jaggy@humans.ph>
2023-08-25 21:08:51 +0800

    CS fixes

--- packages/vue/src/use-frames-store.js
+++ packages/vue/src/use-frames-store.js
@@ -4,6 +4,16 @@ import { defineStore } from "pinia";
 export const useFramesStore = defineStore("inertia-frames", () => {
     const frames = ref([])
 
+    function toggleFrame ($$id) {
+        frames.value = frames.value.map((frame) => {
+            if (frame.$$id === $$id) {
+                frame.active = ! frame.active
+            }
+
+            return frame
+        })
+    }
+
     function open (frame) {
         const transition = frame?.transition || 300
 
@@ -16,25 +26,11 @@ export const useFramesStore = defineStore("inertia-frames", () => {
             ...frame,
         })
 
-        nextTick(() => {
-            frames.value = frames.value.map((frame) => {
-                if (frame.$$id === $$id) {
-                    frame.active = true
-                }
-
-                return frame
-            })
-        })
+        nextTick(() => toggleFrame($$id))
     }
 
     function close ({ transition, $$id }) {
-        frames.value = frames.value.map((frame) => {
-            if (frame.$$id === $$id) {
-                frame.active = false
-            }
-
-            return frame
-        })
+        toggleFrame($$id)
 
         setTimeout(() => {
             frames.value = frames.value.filter(
